7ac2e53707fcf692d38939b9e07f293927178eec
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-09-02 14:14:20 -0500

    feat: actualización de repo

--- src/routes/InfoServices.routes.js
+++ src/routes/InfoServices.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { methods as infoServicesController } from "./../controllers/infoServices.controller"
 
 const router = Router();
-
+console.log("Hola Mundo")
 /* A router that is used to create the routes for the API. */
 router.get("/", infoServicesController.getInfoServices); // GET http://localhost:8000/api-restful/infoServices/
 router.get("/:idInstitucion", infoServicesController.getById); // GET http://localhost:8000/api-restful/infoServices/:idInstitucion
