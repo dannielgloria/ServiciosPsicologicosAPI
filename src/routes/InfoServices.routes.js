4bfee1f00fc4e55b7de506c782f95bd27421d032
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-09-02 14:50:57 -0500

    feat: se actualizaron las rutas

--- src/routes/InfoServices.routes.js
+++ src/routes/InfoServices.routes.js
@@ -4,9 +4,9 @@ import { methods as infoServicesController } from "./../controllers/infoServices
 const router = Router();
 console.log("Hola Mundo")
 /* A router that is used to create the routes for the API. */
-router.get("/", infoServicesController.getInfoServices); // GET http://localhost:8000/api-restful/infoServices/
+router.get("/allInstitutions", infoServicesController.getInfoServices); // GET http://localhost:8000/api-restful/infoServices/
 router.get("/:idInstitucion", infoServicesController.getById); // GET http://localhost:8000/api-restful/infoServices/:idInstitucion
-router.post("/", infoServicesController.addInfoService); // POST http://localhost:8000/api-restful/infoServices/
+router.post("/addInstitution", infoServicesController.addInfoService); // POST http://localhost:8000/api-restful/infoServices/
 router.put("/:idInstitucion", infoServicesController.updateInfoService); // PUT http://localhost:8000/api-restful/infoServices/:idInstitucion
 //router.patch("/:idInstitucion", infoServicesController.updateInfoService); // PATCH http://localhost:8000/api-restful/infoServices/:idInstitucion
 router.delete("/:idInstitucion", infoServicesController.deleteInfoService);// DELETE http://localhost:8000/api-restful/infoServices/:idInstitucion
