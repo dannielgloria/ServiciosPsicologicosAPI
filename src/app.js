40e98f256ec66df2903e7f0e9a3b3a8228b03389
Anahi Garcia Soria <anahi.gsoria@gmail.com>
2022-09-02 14:24:15 -0500

    feat: actualizando app

--- src/app.js
+++ src/app.js
@@ -5,16 +5,20 @@ import infoServicesRoutes from "./routes/InfoServices.routes";
 
 const app = express();
 
-app.set("port",8000);
+app.set('port', (process.env.PORT || 5000));
 
 /*Milddwares*/
 app.use(morgan("dev"));
 app.use(express.json());
 
 /* Root Endpoint */
-app.get('/', (req, res)=>{
-    res.send('Api serviciospsicologicos by Anahi Garcia');
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
+
 
 app.use("/api-restful/infoServices",infoServicesRoutes);
 
